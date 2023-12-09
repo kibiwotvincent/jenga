8b4c1b9206168b13a42a4c548b92496557d01032
Kibiwot Vincent <kibiwotvincent@gmail.com>
2023-12-09 06:15:51 +0300

    add logging

--- app/Http/Controllers/AccountInquiryController.php
+++ app/Http/Controllers/AccountInquiryController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use NjoguAmos\Jenga\Api\AccountInquiry;
 use Illuminate\Support\Facades\Http;
 use Spatie\Crypto\Rsa\PrivateKey;
+use Log;
 
 class AccountInquiryController extends Controller
 {
